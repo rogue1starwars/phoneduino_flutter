3ba304f0f25134e877128c522918655b9779d9d6
tetsuka <rogue1starwars728@yahoo.co.jp>
2024-10-25 09:34:22 +0900

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,130 @@
+import "dart:async";
+
+import 'package:flutter/material.dart';
+import "package:flutter_compass/flutter_compass.dart";
+import "package:geolocator/geolocator.dart";
+
+import "package:permission_handler/permission_handler.dart";
+
+import "package:gps_compass/widgets/ble.dart";
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: GpsCompass(),
+    );
+  }
+}
+
+class GpsCompass extends StatefulWidget {
+  const GpsCompass({super.key});
+
+  @override
+  State<GpsCompass> createState() => _GpsCompassState();
+}
+
+class _GpsCompassState extends State<GpsCompass> {
+  late StreamSubscription<Position> _positionStream;
+  late StreamSubscription<CompassEvent> _compassStream;
+  final LocationSettings locationSettings = const LocationSettings(
+    accuracy: LocationAccuracy.best,
+    distanceFilter: 0,
+  );
+  bool _serviceEnabled = false;
+  bool _GeolocationPermissionGranted = false;
+  bool _OrientationPermissionGranted = false;
+  @override
+  void initState() {
+    super.initState();
+
+    Future(
+      () async {
+        _serviceEnabled = await Geolocator.isLocationServiceEnabled();
+
+        LocationPermission permission = await Geolocator.checkPermission();
+        if (permission == LocationPermission.deniedForever) {
+          return;
+        }
+        if (permission != LocationPermission.denied) {
+          setState(() {
+            _GeolocationPermissionGranted = true;
+          });
+          return;
+        }
+
+        permission = await Geolocator.requestPermission();
+        if (permission != LocationPermission.denied) {
+          setState(() {
+            _GeolocationPermissionGranted = true;
+          });
+          return;
+        }
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            BLEWidget(),
+            _GeolocationPermissionGranted
+                ? StreamBuilder<Position>(
+                    stream: Geolocator.getPositionStream(
+                        locationSettings: locationSettings),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasError) {
+                        return Text("Error: ${snapshot.error}");
+                      }
+
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return CircularProgressIndicator();
+                      }
+
+                      Position? position = snapshot.data;
+
+                      if (position == null) {
+                        return const Text("Unknown");
+                      }
+
+                      return Text(
+                          "Position: ${position.latitude}, ${position.longitude}");
+                    },
+                  )
+                : const Text("Permission not granted"),
+            StreamBuilder(
+              stream: FlutterCompass.events,
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Text("Error: ${snapshot.error}");
+                }
+
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                }
+
+                double? direction = snapshot.data!.heading;
+
+                if (direction == null) {
+                  return const Text("No sensors!");
+                }
+
+                return Text("Direction: $direction");
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/ble.dart
@@ -0,0 +1,149 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+
+class BLEWidget extends StatefulWidget {
+  const BLEWidget({super.key});
+
+  @override
+  State<BLEWidget> createState() => _BLEWidgetState();
+}
+
+class _BLEWidgetState extends State<BLEWidget> {
+  // For Bluetooth availability
+  BluetoothAdapterState _adapterState = BluetoothAdapterState.unknown;
+  late StreamSubscription<BluetoothAdapterState> _adapterStateSubscription;
+
+  // For BLE scanning
+  BluetoothDevice? _scannedResult;
+  List<BluetoothDevice> _systemDevices = [];
+  late StreamSubscription<List<ScanResult>> _scanResultsSubscription;
+
+  // For BLE connection
+  late StreamSubscription<BluetoothConnectionState>
+      _connectionStateSubscription;
+
+  @override
+  initState() {
+    super.initState();
+
+    // Check for BLE availability
+    _adapterStateSubscription =
+        FlutterBluePlus.adapterState.listen((BluetoothAdapterState state) {
+      print(state);
+      _adapterState = state;
+      if (mounted) {
+        setState(() {});
+      }
+    });
+
+    _scanResultsSubscription =
+        FlutterBluePlus.scanResults.listen((List<ScanResult> results) async {
+      print("results");
+      print("results: $results");
+      if (results.isEmpty) {
+        return;
+      }
+      _scannedResult = results[0].device;
+      if (mounted) {
+        setState(() {});
+      }
+      await results[0].device.connect(autoConnect: true, mtu: null);
+      if (mounted) {
+        setState(() {});
+      }
+    });
+
+    // FlutterBluePlus.cancelWhenScanComplete(_scanResultsSubscription);
+
+    if (_scannedResult != null) {
+      _connectionStateSubscription =
+          _scannedResult!.connectionState.listen((state) {
+        if (state == BluetoothConnectionState.disconnected) {
+          _scannedResult!.connect(autoConnect: true, mtu: null);
+          print("Reconnecting...");
+        }
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _adapterStateSubscription.cancel();
+    _scanResultsSubscription.cancel();
+  }
+
+  Future onScanPressed() async {
+    try {
+      await FlutterBluePlus.startScan(timeout: const Duration(seconds: 1));
+    } catch (e) {
+      print(e);
+    }
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  Future onSearchCharacteristics() async {
+    List<BluetoothService> services = await _scannedResult!.discoverServices();
+    services.forEach((service) {
+      print("Service: ${service.uuid}");
+      service.characteristics.forEach((characteristic) async {
+        print(characteristic.uuid);
+        print("Characteristic: ${characteristic.runtimeType}");
+        try {
+          if (characteristic.uuid.str ==
+              "6e400002-b5a3-f393-e0a9-e50e24dcca9e") {
+            print("Writing...");
+            await (characteristic as BluetoothCharacteristic)
+                .write([0x12, 0x34]);
+          }
+        } catch (e) {
+          print(e);
+        }
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        TextButton(
+          onPressed: () async {
+            if (_adapterState != BluetoothAdapterState.on) {
+              return;
+            }
+            // Scan for BLE devices
+            try {
+              await FlutterBluePlus.startScan(
+                  withNames: ["UART Service"],
+                  timeout: const Duration(seconds: 10));
+            } catch (e) {
+              print(e);
+            }
+            if (mounted) {
+              setState(() {});
+            }
+          },
+          child: Text(
+            _adapterState == BluetoothAdapterState.on
+                ? "Bluetooth is on"
+                : "Bluetooth is off",
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+        Text(_scannedResult == null
+            ? "No device found"
+            : _scannedResult!.remoteId.str),
+        TextButton(
+            onPressed: onSearchCharacteristics,
+            child: Text("Search Characteristics")),
+      ],
+    );
+  }
+}
